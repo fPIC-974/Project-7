db9f3e34fbd0e02cb00eaf16851e086e0db5cfd7
Frederic Picard <fred.picard@gmail.com>
2023-08-10 16:36:30 +0200

    Updated Trade : validation ok

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -3,6 +3,7 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.sql.Timestamp;
@@ -28,7 +29,7 @@ public class Trade {
     )
     private String type;
     @Min(value = 0, message = "Positive value only")
-//    @Pattern(regexp = "^(0|([1-9][0-9]*))(\\.[0-9]+)$")
+    @NotNull(message="Buy quantity is mandatory")
     private Double buyQuantity;
     private Double sellQuantity;
     private Double buyPrice;
